75fbceb7c6f5459c3abe861d0f0ec40c53abfbf9
Anne Marie Busch <abusch01@students.uni-mainz.de>
2022-04-06 17:32:15 +0200

    added pairwise correlation, plots are downloadable as pdf and pptx, added progress bar during pairwise correlation

--- app.R
+++ app.R
@@ -7,88 +7,80 @@ library(gridExtra)
 library(officer)
 library(openxlsx)
 # library(patchwork)
+library(reporter)
 library(shiny)
 library(shinydashboard)
 
+# install.packages("reporter", dependencies = T)
+# library(BiocManager)
+# BiocManager::install('mixOmics')
 # if(interactive()){
+
 ui <- fluidPage(
     # theme = bslib::bs_theme(bootswatch = "flatly"),
     # titlePanel("First Version"),
-    dashboardPage(
-        skin = "purple",
-        dashboardHeader(title = "Basic Dashboard"), 
-        dashboardSidebar(
-            sidebarMenu(
-                menuItem("Import Data", tabName = "importdata"), 
-                menuItem("Correlation Matrices", tabName = "correlationmatrices")
+  dashboardPage(
+    skin = "purple",
+    dashboardHeader(title = "Behavior Analysis"), 
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Import Data", tabName = "importdata"), 
+        menuItem("Analysis", tabName = "analysis", 
+                 menuSubItem("Correlation Matrices", tabName = "correlationmatrices"), 
+                 menuSubItem("Pairwise Correlations", tabName = "pairwisecorrelations")
+        )       
+      )
+    ),
+    dashboardBody(
+      tabItems(
+        tabItem(tabName = "importdata", 
+                fluidRow(
+                  column(12,
+                         fileInput("upload", "Select file to input", accept=".xlsx", placeholder = ".xlsx input file"), 
+                         dataTableOutput("datatable")
+                  )
+                )), 
+        tabItem(tabName = "correlationmatrices", 
+          fluidRow(
+            column(12, 
+              h4("Select Variables"), 
+              div(style = "height:300px;overflow-y: scroll", uiOutput("selectlabels")), 
             )
+          ),
+          fluidRow(
+            column(12, 
+                 h4("Correlation Matrix"), 
+                 div(style = "display:inline-block", actionButton("plot_selected_script", "Plot")), 
+                 div(style = "display:inline-block", downloadButton("downloadcorr", "Download")),
+                 # div(style= "display:inline-block", radioButtons("dfcorrmatrix", "", c(".pdf", ".pptx"), inline = TRUE)),
+                 plotOutput("cormat", height=800)
+            )
+          ), 
         ), 
-        dashboardBody(
-            tabItems(
-                tabItem(tabName = "importdata", 
-                        fluidRow(
-                            column(12,
-                                fileInput("upload", "Select file to input", accept=".xlsx", placeholder = ".xlsx input file"), 
-                                dataTableOutput("datatable")
-                            )
-                        )),
-                tabItem(tabName = "correlationmatrices", 
-                        fluidRow(
-                            column(12, 
-                                h4("Select Variables"),
-                                div(style = "height:300px;overflow-y: scroll", uiOutput("selectlabels")), 
-                            )
-                        ),
-                        fluidRow(
-                            column(12, 
-                                h4("Correlation Matrix"), 
-                                actionButton("plot_selected", "Plot"),
-                                downloadButton("downloadcorr", "Download"),
-                                plotOutput("cormat", height=800)
-                            )
-                        )
-                        )
-                )
-           # tabsetPanel(id = "wizard", type = "hidden",
-        # tabPanel("page_1",
-            #fileInput("upload", "Select file to input", accept=".xlsx", placeholder = ".xlsx input file"),
-            # dataTableOutput("datatable"), 
-            # actionButton("page_12", "next")
-        # ), 
-        # tabPanel("page_2", 
-            # verbatimTextOutput("textfield"),
-            # checkboxGroupInput("corr_variables", "Which variables do you want to plot?", "all"),
-            # uiOutput("selectlabels"),
-            # actionButton("plot_selected", "Plot"),
-            # plotOutput("cormat", height=800),
-            # downloadButton("downloadcorr", "Download plot"),
-            # actionButton("page_21", "prev"),
-            # actionButton("page_23", "next")
-        # ),
-        # tabPanel("page_3", 
-           
-            
-            # actionButton("page_32", "prev"),
-            # actionButton("page_34", "next")
-        # ), 
-        # tabPanel("page_4",
-            # plotOutput("paircorr"),
-            # actionButton("page_43", "prev"))
-    # )
-        # )
-    # )
-    # sidebarLayout(position = "left", 
-        #sidebarPanel(
-    
-    #       ),
-    #    mainPanel(
+        tabItem(tabName = "pairwisecorrelations", 
+          fluidRow(
+            column(12, 
+              h4("Pairwise Correlations"),
+              selectInput("corrtype", "Correlation Type", choices = c("pearson")),# , "spearman", "kendall")),
+              downloadButton("downpaircorr", "Download"),
+              div(style= "display:inline-block", radioButtons("dfpairmatrix", "", c(".pdf", ".pptx"), inline = TRUE))
+              # verbatimTextOutput("selected_format"),
+              # progress bar 
+            )
+          )
+        )
+      )
+    )
+  )
+)
+                
+         
 
         
-        )
-    )
-)
 
 server <- function(input, output, session) {
+    source("prelim_script.R")
+  
     labels <- reactive({
         req(infile <- input$upload)
         read.xlsx(infile$datapath, "labels", colNames = T)
@@ -108,17 +100,8 @@ server <- function(input, output, session) {
         # returns the data if all tests are passed
         return(dat)
     })
-    # switch_page <- function(i){
-    #     updateTabsetPanel(inputId = "wizard", selected = paste0("page_", i))
-    # }
-    
-    # observeEvent(input$page_12, switch_page(2))
-    # observeEvent(input$page_21, switch_page(1))
-    # observeEvent(input$page_32, switch_page(2))
-    # observeEvent(input$page_23, switch_page(3))
-    # observeEvent(input$page_34, switch_page(4))
-    # observeEvent(input$page_43, switch_page(3))
-    
+
+    output$selected_format <- renderPrint(input$dfcorrmatrix)    
     
     output$selectlabels <- renderUI({
         checkboxGroupInput("selection", "Which data do you want to plot?", choiceNames = paste(labels()$label1, labels()$label2), choiceValues=labels()$colnames, selected=labels()$colnames) 
@@ -126,97 +109,128 @@ server <- function(input, output, session) {
     output$datatable <- renderDataTable({
         inputdata()[, 1:ncol(inputdata())] # rownames = T, colnames = T
         })
-    plot_cormat <- function(selectedLabels){ 
-        # elect <- req(input$selection)
-        # cormat <- rcorr(as.matrix(inputdata()[,1:ncol(inputdata())]), type = "pearson")
-        cormat <- rcorr(as.matrix(inputdata()[, c(paste0(selectedLabels))]), type = "pearson")
-        cormat_r <- cormat$r
-        cormat_r[cormat$P > 0.05] <- 0
-        cormat_plot <- corrplot(cormat_r, tl.col = "black", tl.srt = 60, tl.cex = 0.6, mar = c(0,0,2,2))
-        return(cormat_plot)
-        }
     
-    observeEvent(input$plot_selected,
-        {plotvalues <- input$selection
-        output$cormat <- renderPlot({plot_cormat(plotvalues)}, res = 96)}
-        )
-    
+    observeEvent(input$plot_selected_script, 
+        {plotvalues <- c(paste0(input$selection))
+        source("prelim_script.R", local = TRUE)
+        output$cormat <- renderPlot(correlationMatrix(inputdata(), subset = plotvalues, outDir = NULL, filename = NULL))
+         })
+
     output$downloadcorr <- downloadHandler( 
-        # opens pdf in new window on my machine and when exited, doesn't return to app
-        # works good, when run directly in a window. possibly something up with my settings
         filename <- ("cormat.pdf"), 
         content <- function(file) { 
-            # this might not work when running on shiny server
+            plotvalues <- c(paste0(input$selection))
             pdf(file, height = 10, width = 11, paper = "a4")
-            plot_cormat(input$selection)
+            corrmatrix <- correlationMatrix(inputdata(), subset = plotvalues, outDir = NULL, filename = NULL)
             dev.off()
-            }
+            } 
         )
-    output$paircorr <- renderPlot({
-      
-        threshold = 0.0001
-        
-        lplots <- list()
-        index <- 1
-        # doc <- read_pptx()
-        for (i in 1:(ncol(inputdata())-1)){
-            for (j in (i+1):ncol(inputdata())){
-                #Calculate correlations 
-                p.val <- cor.test(inputdata()[,i], inputdata()[,j])$p.value
-                r <- round(cor.test(inputdata()[,i], inputdata()[,j])$estimate,2)
-
-                #Create a plot if the correlation is significant
-                if(p.val<=threshold){
-                    
-                    x_lab <- paste(labels()$label1[i], 
-                                   labels()$label2[i], sep ="\n")
-                    
-                    y_lab <- paste(labels()$label1[j], 
-                                   labels()$label2[j], sep ="\n")
-                    
-                    p <- ggplot(inputdata(), 
-                                aes(x = inputdata()[,i], 
-                                    y = inputdata()[,j])) +
-                        geom_point(size=2.5) +
-                        # stat_smooth(formula = "y~x", method = "lm", se = F, color="black") +
-                        labs(col="", x = x_lab, y = y_lab) +
-                        theme_test(base_size=14) + theme(axis.text=element_text(size=12, color="black")) 
-                    
-                    ymax = layer_scales(p)$y$range$range[2]
-                    ymin = layer_scales(p)$y$range$range[1]
-                    
-                    p <- p + ylim(ymin, 1.4*ymax)
-                    xcoord <- layer_scales(p)$x$range$range[2] - (layer_scales(p)$x$range$range[2] - layer_scales(p)$x$range$range[1])/2
-                    # writeLines(paste0(xcoord))
-                    if(p.val<0.001){
-                        lab1 <- paste0("r=",  r, ", p<0.001")
-                    }else{
-                        lab1 <- paste0("r=",  r, ", p=" ,round(p.val,3))
-                    }
-                    
-                    p <- p+ annotate(geom = "text", x = xcoord, y = 1.2*ymax,
-                                     label=lab1, fontface=3, size=4.5)
-                     #if (p){
-                    p <- list(p)
-                    lplots <- append(lplots, p)
-                    # lplots[[index]] <- new[p
-                    # index <- index + 1
-                    # }
-                    # return(p)
-                    writeLines(paste0(length(lplots)))  
-                    writeLines(paste0(typeof(lplots[[index]])))
-                }
-            }
+  
+    output$downpaircorr <- downloadHandler( 
+      filename <- function () {
+        # writeLines(input$dfpairmatrix)
+        if (input$dfpairmatrix == ".pptx"){
+          name <- "pair_corr_plots.pptx" 
+          # writeLines(name)
+          return(name)
+        } else {
+          name <- "pair_corr_plots.pdf"
+          return(name)
         }
-    grid.arrange(grobs=lapply(lplots), ncol=3, top="Pairwise Correlation")
-    # return(lplots)
-    # wrap_plots(lplots)
+      },
+      content <- function(file) { 
+        format <- input$dfpairmatrix
+        data_table <- inputdata()
+        labels <- labels()
+        type <- input$corrtype
+        # writeLines(type)
+        threshold <- 0.05 
+        grouping <- NULL 
+        color_groups <- NULL 
+        # subset = NULL
+        #Determine the output format selected by the user 
+        if(format == ".pdf"){
+          pdf(file, height=4.5, width = 5.5)
+        }
+        # writeLines(file)
+        if(format == ".pptx"){
+          doc <- officer::read_pptx()
+        }
+        
+        progress <- shiny::Progress$new()
+        on.exit(progress$close())
+        progress$set(message = "Making plots", value = 0)
+        #Calculate the correlations and create the plots
+        for (i in 1:(ncol(data_table)-1)){
+        # for(i in 1:5){
+          progress$inc(1/(ncol(data_table)-1))
+          for (j in (i+1):ncol(data_table)){
+          # for (j in (i+1):5){  
+            #Calculate correlations 
+            p.val = stats::cor.test(data_table[,i], data_table[,j], method = type)$p.value
+            r = round(stats::cor.test(data_table[,i], data_table[,j], method = type)$estimate,2)
+            
+            #Create a plot if the correlation is significant
+            if(p.val<=threshold){
+              
+              x_lab = paste(labels$label1[i], 
+                            labels$label2[i], sep ="\n")
+              
+              y_lab = paste(labels$label1[j], 
+                            labels$label2[j], sep ="\n")
+              
+              p = ggplot2::ggplot(data_table, 
+                                  aes(x = data_table[,i], 
+                                      y = data_table[,j])) +
+                geom_point(size=2.5, col = "black") +
+                stat_smooth(formula = "y~x", method = "lm", se = F, color="black") +
+                labs(col="", x = x_lab, y = y_lab) +
+                theme_classic(base_size=14) + theme(axis.text=element_text(size=12, color="black")) 
+              
+              
+              #Include grouping color if included
+              if(!is.null(grouping)){
+                grouping = factor(grouping)
+                p = p + geom_point(size  = 2.5, aes(col = grouping)) 
+                
+                if(!is.null(color_groups) & length(color_groups) >= length(unique(grouping))){
+                  p <- p + scale_color_manual(values = color_groups)
+                }
+              }
+              
+              #Get y-limits of the plotting area
+              ymax = ggplot2::layer_scales(p)$y$range$range[2]
+              ymin = ggplot2::layer_scales(p)$y$range$range[1]
+              
+              #Increase the limits of the plot to include the r and p-value
+              p = p + ylim(ymin, 1.4*ymax)
+              xcoord = layer_scales(p)$x$range$range[2] - (layer_scales(p)$x$range$range[2] - layer_scales(p)$x$range$range[1])/2
+              
+              if(p.val<0.001){
+                lab1 = paste0("r=",  r, ", p<0.001")
+              }else{
+                lab1 = paste0("r=",  r, ", p=" ,round(p.val,3))
+              }
+              
+              p = p+ ggplot2::annotate(geom = "text", x = xcoord, y = 1.2*ymax,
+                                       label=lab1, fontface=3, size=4.5)
+              
+              if(format == ".pptx"){
+                doc <- officer::add_slide(doc)
+                doc <- officer::ph_with(x = doc, value = p, location =ph_location(type="body",width=6, height=4.5), res=600)
+              } else {print(p)}
+              
+            }
+          }
+        }
+        if(format == ".pptx"){
+          print(doc, target = file) 
+        } else {dev.off()}
+      } 
+     
+      )
     
-    })
 
-    # output$plot <- renderPlot({
-    #    plot(dataset())
-    # }, res = 96)
     
     }
 
--- prelim_script.R
+++ prelim_script.R
@@ -7,19 +7,17 @@ library(pheatmap)
 library(mclust)
 library(openxlsx)
 library(mice)
-# library(ggpubr)
+library(ggpubr)
 library(nlme)
 library(lme4)
 library(emmeans)
 library(car)
 library(gam)
-# library(mixOmics)
+library(mixOmics)
 library(clustvarsel)
 library(PMCMRplus)
 library(ggrepel)
 library(officer)
-# install.packages("mixOmics", dependencies = T)
-# install.packages("curl", dependencies = T)
 
 ###########################################################################
 #                              IMPORT DATA
@@ -27,7 +25,7 @@ library(officer)
 #Either set directory to the folder containing the files or write the full directory to the files
 #setwd("/Directory/To/Files)
 
-setwd("/mnt/sda2/Dokumente/Bioinformatik/6.Semester/Behavior_GUI")
+
 data_table  <- read.xlsx("grand_table.xlsx", colNames = T, sheet =  "grand_table", rowNames = T)
 labels <- read.xlsx("grand_table.xlsx", colNames = T, sheet =  "labels")
 meta_data <- read.xlsx("grand_table.xlsx", colNames = T, sheet =  "meta_data") 
@@ -52,69 +50,168 @@ all(vec) == TRUE
 #
 ################################################################################################################
 #CORRELATION MATRIX ALL VALIABLES
+#Function to calculate a correlation matrix for the behavioural data matrix
 
-cormat <- rcorr(as.matrix(data_table[,1:ncol(data_table)]), type = "pearson")
-cormat_r <- cormat$r
-cormat_r[cormat$P > 0.05] <- 0
+#ARGUMENTS
+#data_table - the input data frame 
+#outDir - the output directory where the file will be saved
+#filename - name of the output file
+#type - type of the correlation, by default "pearson", can be changed to "spearman" for non-parametric correlation
+#threshold - the cutoff p-value for considering a correlation to be statistically significant, by default 0.05
+#subset - a vector containing column indices in case the user once to calculate the matrix for a subset of the data
 
-#Export correlation matrix
-pdf("cormat.pdf",height = 10, width = 11)
-corrplot(cormat_r, tl.col = "black", tl.srt = 60, tl.cex = 0.6, mar = c(0,0,2,2))
-dev.off()
+
+correlationMatrix <- function(data_table, outDir, 
+                              filename, type = "pearson", 
+                              threshold  = 0.05, subset = NULL){
+  
+  if(!is.null(subset)){
+    cormat = Hmisc::rcorr(as.matrix(data_table[,subset]), type = type) 
+  }else{
+    cormat = Hmisc::rcorr(as.matrix(data_table[,1:ncol(data_table)]), type = type)
+  }
+  
+  #Extract the correlation coefficients
+  cormat_r = cormat$r
+  #Set non-significant coefficients to 0
+  cormat_r[cormat$P > threshold] = 0
+  
+  #Export correlation matrix
+  if(ncol(cormat_r) < 10){
+    height = 5
+    width = 6
+    font_size = 1
+  }
+  
+  if(ncol(cormat_r) > 10 & ncol(cormat_r) < 50){
+    height = 7
+    width = 8
+    font_size = 0.8
+  }
+  
+  if(ncol(cormat_r)>=50 & ncol(cormat_r)<90){
+    height = 9
+    width = 10
+    font_size=0.7
+  }
+  
+  if(ncol(cormat_r)>=90 ){
+    height = 10
+    width = 11
+    font_size=0.6
+  }
+  
+  # pdf(paste0(outDir, "/", filename, ".pdf"), height = height, width = width)
+  corrplot::corrplot(cormat_r, tl.col = "black", tl.srt = 60, tl.cex = font_size, mar = c(0,0,2,2))
+  # dev.off()
+  
+  
+}
+
+
 
 
 ##############################################################################
-#Calculate pairwise correlations (all or only significant, controlled by the threshold variable)
-library(officer)
-threshold = 0.0001
+#Function to calculate pairwise correlations (all or only significant, controlled by the threshold variable)
+#ARGUMENTS
+#data_table - the input data frame 
+#labels - data frame with labels
+#outDir - the output directory where the file will be saved
+#filename - name of the output file
+#format -  the file format for the output, per default a pdf with the correlations is created, optionally, plots can be saved in a power point file
+#type - type of the correlation, by default "pearson", can be changed to "spearman" or "kendall" for non-parametric correlation
+#threshold - the cutoff p-value for considering a correlation to be statistically significant, by default 0.05
+#grouping - a grouping variable cab be included to color data points according to a group assignment
+#color_groups - a vector with as many values as levels of the "grouping" argument
+#subset - a vector containing column indices in case the user once to calculate the matrix for a subset of the data
 
-doc <- read_pptx()
-for (i in 1:(ncol(data_table)-1)){
-  for (j in (i+1):ncol(data_table)){
-    
-    #Calculate correlations 
-    p.val <- cor.test(data_table[,i], data_table[,j])$p.value
-    r <- round(cor.test(data_table[,i], data_table[,j])$estimate,2)
-    
-    #Create a plot if the correlation is significant
-    if(p.val<=threshold){
-      
-      x_lab <- paste(labels$label1[i], 
-                     labels$label2[i], sep ="\n")
-      
-      y_lab <- paste(labels$label1[j], 
-                     labels$label2[j], sep ="\n")
-      
-      p <- ggplot(data_table, 
-                  aes(x = data_table[,i], 
-                      y = data_table[,j])) +
-        geom_point(size=2.5) +
-        stat_smooth(formula = "y~x", method = "lm", se = F, color="black") +
-        labs(col="", x = x_lab, y = y_lab) +
-        theme_test(base_size=14) + theme(axis.text=element_text(size=12, color="black")) 
-      
-      ymax = layer_scales(p)$y$range$range[2]
-      ymin = layer_scales(p)$y$range$range[1]
-     
-       p <- p + ylim(ymin, 1.4*ymax)
-      xcoord <- layer_scales(p)$x$range$range[2] - (layer_scales(p)$x$range$range[2] - layer_scales(p)$x$range$range[1])/2
-      
-      if(p.val<0.001){
-        lab1 <- paste0("r=",  r, ", p<0.001")
-      }else{
-        lab1 <- paste0("r=",  r, ", p=" ,round(p.val,3))
-      }
-      
-      p <- p+ annotate(geom = "text", x = xcoord, y = 1.2*ymax,
-                       label=lab1, fontface=3, size=4.5)
-      
-      
-      doc <- add_slide(doc)
-      doc <- ph_with(x = doc, p, ph_location(type="body",width=6, height=4.5), res=600)
-    }
-    
+pairwiseCorrelations <- function(data_table, labels, outDir, filename, format = ".pdf", 
+                                 type = "pearson", threshold = 0.05, 
+                                 grouping = NULL, color_groups = NULL, 
+                                 subset = NULL){
+  
+  #Subset the input data frame and the labels data frame if a subset argument is provided
+  if(!is.null(subset)){
+    data_table <- data_table[,subset]
+    labels <- labels[subset,]
   }
+  
+  #Determine the output format selected by the user 
+  if(format == ".pdf"){
+    pdf(paste0(outDir, "/", filename, ".pdf"), height=4.5, width = 5.5)
+  }
+  
+  if(format == ".pptx"){
+    doc <- officer::read_pptx()
+  }
+  
+  #Calculate the correlations and create the plots
+  for (i in 1:(ncol(data_table)-1)){
+    for (j in (i+1):ncol(data_table)){
+      
+      #Calculate correlations 
+      p.val = stats::cor.test(data_table[,i], data_table[,j], method = type)$p.value
+      r = round(stats::cor.test(data_table[,i], data_table[,j], method = type)$estimate,2)
+      
+      #Create a plot if the correlation is significant
+      if(p.val<=threshold){
+        
+        x_lab = paste(labels$label1[i], 
+                       labels$label2[i], sep ="\n")
+        
+        y_lab = paste(labels$label1[j], 
+                       labels$label2[j], sep ="\n")
+        
+        p = ggplot2::ggplot(data_table, 
+                    aes(x = data_table[,i], 
+                        y = data_table[,j])) +
+          geom_point(size=2.5, col = "black") +
+          stat_smooth(formula = "y~x", method = "lm", se = F, color="black") +
+          labs(col="", x = x_lab, y = y_lab) +
+          theme_classic(base_size=14) + theme(axis.text=element_text(size=12, color="black")) 
+        
+        
+        #Include grouping color if included
+        if(!is.null(grouping)){
+          grouping = factor(grouping)
+          p = p + geom_point(size  = 2.5, aes(col = grouping)) 
+          
+          if(!is.null(color_groups) & length(color_groups) >= length(unique(grouping))){
+            p <- p + scale_color_manual(values = color_groups)
+          }
+        }
+        
+        
+        #Get y-limits of the plotting area
+        ymax = ggplot2::layer_scales(p)$y$range$range[2]
+        ymin = ggplot2::layer_scales(p)$y$range$range[1]
+        
+        #Increase the limits of the plot to include the r and p-value
+        p = p + ylim(ymin, 1.4*ymax)
+        xcoord = layer_scales(p)$x$range$range[2] - (layer_scales(p)$x$range$range[2] - layer_scales(p)$x$range$range[1])/2
+        
+        if(p.val<0.001){
+          lab1 = paste0("r=",  r, ", p<0.001")
+        }else{
+          lab1 = paste0("r=",  r, ", p=" ,round(p.val,3))
+        }
+        
+        p = p+ ggplot2::annotate(geom = "text", x = xcoord, y = 1.2*ymax,
+                         label=lab1, fontface=3, size=4.5)
+        
+        
+        if(format == ".pptx"){
+        doc = officer::add_slide(doc)
+        doc = officer::ph_with(x = doc, p, ph_location(type="body",width=6, height=4.5), res=600)
+        }else{print(p)}
+        
+      }
+    }
+  }
+  
+  if(format == ".pptx"){
+   print(doc, target = paste0(outDir, "/", filename,".pptx")) 
+  }else(dev.off())
+  
 }
-
-print(doc, target = "pairwise_cor_plots.pptx")
 
